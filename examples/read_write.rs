28f4a7a6437dafd368486e6f5487ec1f7212762c
jabu <jabu@gx.ag>
2024-05-10 20:38:49 -0500

    add ReadWrite type as an alternative to Socket trait

---
+++ examples/read_write.rs
@@ -0,0 +1,85 @@
+use std::time::Duration;
+
+use rusty_enet as enet;
+
+fn main() {
+    let mut host1 = enet::Host::new(
+        enet::ReadWrite::<(), enet::Error>::new(),
+        enet::HostSettings::default(),
+    )
+    .unwrap();
+    let mut host2 = enet::Host::new(
+        enet::ReadWrite::<(), enet::Error>::new(),
+        enet::HostSettings::default(),
+    )
+    .unwrap();
+
+    macro_rules! update {
+        () => {
+            for _ in 0..10 {
+                std::thread::sleep(Duration::from_millis(1));
+                update_host("Host 1", &mut host1, &mut host2);
+                update_host("Host 2", &mut host2, &mut host1);
+            }
+        };
+    }
+
+    host1.connect((), 255, 100).unwrap();
+
+    update!();
+
+    host1
+        .peer_mut(enet::PeerID(0))
+        .send(0, &enet::Packet::reliable("Hello!".as_bytes()))
+        .unwrap();
+
+    update!();
+
+    host1.peer_mut(enet::PeerID(0)).disconnect(32);
+
+    update!();
+}
+
+fn update_host(
+    name: &str,
+    host: &mut enet::Host<enet::ReadWrite<(), enet::Error>>,
+    other_host: &mut enet::Host<enet::ReadWrite<(), enet::Error>>,
+) {
+    while let Some(event) = host.service().unwrap() {
+        match event {
+            enet::Event::Connect { peer, data } => {
+                println!(
+                    "[{}] Connected to {:?} with data: {}",
+                    name,
+                    peer.id(),
+                    data
+                );
+            }
+            enet::Event::Disconnect { peer, data } => {
+                println!(
+                    "[{}] Disconnected from {:?} with data: {}",
+                    name,
+                    peer.id(),
+                    data
+                );
+            }
+            enet::Event::Receive {
+                peer,
+                packet,
+                channel_id,
+            } => {
+                let message = std::str::from_utf8(packet.data()).unwrap();
+                println!(
+                    "[{}] Received message from {:?} on channel {}: {}",
+                    name,
+                    peer.id(),
+                    channel_id,
+                    message
+                );
+            }
+        }
+    }
+    if let Some((_, packet)) = host.socket_mut().read() {
+        other_host.socket_mut().write((), packet);
+    }
+}
---
+++ src/read_write.rs
@@ -0,0 +1,85 @@
+use std::collections::VecDeque;
+
+use crate::{Address, PacketReceived, Socket};
+
+/// Provides a Read/Write interface for use with [`Host`](`crate::Host`).
+///
+/// This provides a useful alternative to implementing the [`Socket`] trait, especially when
+/// interfacing with multiple kinds of sockets at once.
+///
+/// ```
+/// use rusty_enet::{Error, Host, HostSettings, ReadWrite};
+///
+/// let mut host = Host::new(ReadWrite::<(), Error>::new(), HostSettings::default()).unwrap();
+///
+/// // Write packets to the host (usually from one or more sockets)
+/// host.socket_mut().write((/*some address*/), vec![]);
+///
+/// // Read packets ENet wants to send, then send to sockets based on the address
+/// if let Some((address, packet)) = host.socket_mut().read() {
+///     dbg!((address, packet));
+/// }
+/// ```
+#[derive(Debug)]
+pub struct ReadWrite<A: Address + 'static, E: std::error::Error + Send + Sync + 'static> {
+    inbound: VecDeque<(A, Vec<u8>)>,
+    outbound: VecDeque<(A, Vec<u8>)>,
+    error: Option<E>,
+}
+
+impl<A: Address + 'static, E: std::error::Error + Send + Sync + 'static> ReadWrite<A, E> {
+    /// Create an intermediate Read/Write socket for use with [`Host`](`crate::Host`).
+    #[must_use]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Write packets to the ENet host.
+    pub fn write(&mut self, address: A, buffer: Vec<u8>) {
+        self.inbound.push_back((address, buffer));
+    }
+
+    /// Read packets from the ENet host.
+    pub fn read(&mut self) -> Option<(A, Vec<u8>)> {
+        self.outbound.pop_front()
+    }
+
+    /// Send an error to the ENet host.
+    pub fn error(&mut self, error: E) {
+        self.error = Some(error);
+    }
+}
+
+impl<A: Address + 'static, E: std::error::Error + Send + Sync + 'static> Default
+    for ReadWrite<A, E>
+{
+    fn default() -> Self {
+        Self {
+            inbound: VecDeque::new(),
+            outbound: VecDeque::new(),
+            error: None,
+        }
+    }
+}
+
+impl<A: Address + 'static, E: std::error::Error + Send + Sync + 'static> Socket
+    for ReadWrite<A, E>
+{
+    type PeerAddress = A;
+    type Error = E;
+
+    fn send(&mut self, address: A, buffer: &[u8]) -> Result<usize, E> {
+        self.outbound.push_back((address, buffer.to_vec()));
+        Ok(buffer.len())
+    }
+
+    fn receive(&mut self, _mtu: usize) -> Result<Option<(A, PacketReceived)>, E> {
+        if let Some(error) = self.error.take() {
+            Err(error)
+        } else if let Some((address, buffer)) = self.inbound.pop_front() {
+            Ok(Some((address, PacketReceived::Complete(buffer))))
+        } else {
+            Ok(None)
+        }
+    }
+}
